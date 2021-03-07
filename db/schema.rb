778ed3df2a051462441b218563b86ab186b8a668
rysk1013 <rysk2908@gmail.com>
2021-03-07 22:18:26 +0900

    usersテーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_073628) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.text "profile", null: false
+    t.text "profile"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
