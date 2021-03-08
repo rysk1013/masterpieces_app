510fc884648d48e159408fb7c03f206796960bda
rysk1013 <rysk2908@gmail.com>
2021-03-09 00:57:46 +0900

    edit,updateアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:image, :title, :overview, :use_tech, :portfolio_url, :github_url).merge(user_id: current_user.id)
