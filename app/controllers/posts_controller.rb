2aae4d5819e63fe0e500f6b10ecfa4f45760e2b4
rysk1013 <rysk2908@gmail.com>
2021-03-08 19:46:09 +0900

    new,createアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.includes(:user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
