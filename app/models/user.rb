201fde589d1efc5db126d6360563935cf34930ee
rysk1013 <rysk2908@gmail.com>
2021-03-08 12:26:17 +0900

    userモデルバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, {with: PASSWORD_REGEX, message: 'Include both letters and numbers'}
+
   validates :nickname, presence: true, uniqueness: true
 end
