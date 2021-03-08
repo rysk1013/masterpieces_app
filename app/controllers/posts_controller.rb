ab6bfff21ac2e790df0b36781c6b62247d8e1f2a
rysk1013 <rysk2908@gmail.com>
2021-03-09 01:03:47 +0900

    showアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.includes(:user)
   end
@@ -16,12 +17,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to root_path
     else
@@ -32,5 +34,9 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:image, :title, :overview, :use_tech, :portfolio_url, :github_url).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
