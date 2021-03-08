80ed8ad223197564ba446e51d4e6a677bf9416a8
rysk1013 <rysk2908@gmail.com>
2021-03-09 00:43:45 +0900

    postモデルバリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :overview
+    validates :use_tech
+    validates :portfolio_url
+    validates :github_url
+  end
 end
