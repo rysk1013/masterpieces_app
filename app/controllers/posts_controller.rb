ede67f31da4972d9e119165f8d9e601e2f3d2c07
rysk1013 <rysk2908@gmail.com>
2021-03-09 15:53:14 +0900

    destroyアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.includes(:user)
   end
@@ -29,6 +29,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
