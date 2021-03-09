0194ee5f63c90ce62b48e64b996b59afd5953f2c
rysk1013 <rysk2908@gmail.com>
2021-03-09 15:11:40 +0900

    postsコントローラーのストロングパラメーター修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:image, :title, :overview, :use_tech, :portfolio_url, :github_url).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :title, :overview, :programming_languages, :techs, :portfolio_url, :github_url).merge(user_id: current_user.id)
   end
 
   def set_post
