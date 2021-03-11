611d5d5bd805e64f634ef1792530e3015dfa05db
rysk1013 <rysk2908@gmail.com>
2021-03-11 18:26:18 +0900

    posts/indexアクションいいね数の取得

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
+    @likes = Like.includes(:post)
   end
 
   def new
