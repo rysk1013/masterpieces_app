db8f8973fd46bdf536be88ce18eb0d3bd08b0f71
rysk1013 <rysk2908@gmail.com>
2021-03-11 11:44:59 +0900

    comments/createアクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
+    else
+      @comments = @post.comments.includes(:user)
+      render "posts/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
