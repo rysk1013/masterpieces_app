173af506894c97b11a8b251e66e302a6c6d3cc24
rysk1013 <rysk2908@gmail.com>
2021-03-13 19:47:59 +0900

    posts/createアクションのタグ保存設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,9 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    tag_list = params[:post][:tag_name].split(nil)
     if @post.save
+      @post.save_tag(tag_list)
       redirect_to root_path
     else
       render :new
