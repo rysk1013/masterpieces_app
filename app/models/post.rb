f93b6f38743b5e50283f9a16edcedd3feee09f95
rysk1013 <rysk2908@gmail.com>
2021-03-13 19:56:37 +0900

    save_tagインスタンスメソッドの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,19 @@ class Post < ApplicationRecord
     validates :portfolio_url
     validates :github_url
   end
+
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.post_tags.delete Tag.find_by(tag_name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = PostTag.find_or_create_by(tag_name: new)
+      self.post_tags << new_post_tag
+    end
+  end
 end
