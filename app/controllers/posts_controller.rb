02b266dab3dd13c47bc2cdba6781a53eaa3bab58
rysk1013 <rysk2908@gmail.com>
2021-03-14 00:51:59 +0900

    タグ表示の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
     @likes = Like.includes(:post)
-    @tag_list = Tag.includes(:post)
+    @tag_list = Tag.all
   end
 
   def new
