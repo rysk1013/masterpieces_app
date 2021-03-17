52074549b0e0740c1d3e524b5e0893148cc57abd
rysk1013 <rysk2908@gmail.com>
2021-03-17 16:03:42 +0900

    メソッド、before_actionの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_already_like, only: [:show], if: :user_signed_in?
 
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
@@ -26,8 +27,6 @@ class PostsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
-    @already_like = Like.find_by(post_id:params[:id], user_id: current_user.id)
-    @likes = Like.includes(:post)
     @post_tags = @post.tags
   end
 
@@ -62,5 +61,9 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def set_already_like
+    @already_like = Like.find_by(post_id:params[:id], user_id: current_user.id)
   end
 end
