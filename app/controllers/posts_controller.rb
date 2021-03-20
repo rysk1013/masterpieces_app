0a8d70881bd5d173fcdd9e83df50f8d1ca20d753
rysk1013 <rysk2908@gmail.com>
2021-03-21 01:02:55 +0900

    postsコントローラー、ビューの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :set_already_like, only: [:show], if: :user_signed_in?
 
